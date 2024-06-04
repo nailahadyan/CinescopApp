27ec328e7e05d996ec993599a67ed5b34b210a76
nailahadayan <nailahadyan19@gmail.com>
2024-06-04 07:30:59 +0700

    kelompok 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,5 +70,4 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("com.github.bumtech.glide:glide:4.12.0")
 }
